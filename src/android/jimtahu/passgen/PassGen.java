ad37c4017009abbade666491aaabba29e27c5817
jimtahu <jimtahu@gmail.com>
2012-04-03 00:36:09 -0500

    Added lables and button.

--- src/android/jimtahu/passgen/PassGen.java
+++ src/android/jimtahu/passgen/PassGen.java
@@ -2,14 +2,22 @@ package android.jimtahu.passgen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class PassGen extends Activity
-{
+public class PassGen extends Activity{
+
+    /** handles clicks */
+    public void passgen(View v){
+	TextView output = (TextView) this.findViewById(R.id.output);
+	output.setText("Fishy!");
+    }; 
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
-}
+    }//end onCreate
+}//end PassGen
