4d37e10553656ba1119cb38672ec7028bc9e527d
jimtahu <jimtahu@gmail.com>
2012-06-02 18:39:19 -0500

    added test of connecting to a host

--- src/android/jimtahu/passgen/PassGen.java
+++ src/android/jimtahu/passgen/PassGen.java
@@ -5,9 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.OutputStreamWriter;
 
 public class PassGen extends Activity{
 
+    /** connects for a string */
+    public void pulldata(View v){
+        String msg = "fish";
+        EditText output = (EditText) this.findViewById(R.id.output);
+        try{
+            Socket datacom = new Socket(output.getText().toString(), 7310);
+            Scanner input = new Scanner(datacom.getInputStream());
+            OutputStreamWriter host = new OutputStreamWriter(datacom.getOutputStream());
+            msg = input.nextLine();
+            host.write("fish have landed");
+            datacom.close();
+        }catch(Exception ex){
+            msg = ex.toString();
+            ex.printStackTrace();
+        }//end try catch
+        output.setText(msg);
+    };
+    
     /** handles clicks */
     public void passgen(View v){
 	EditText output = (EditText) this.findViewById(R.id.output);
