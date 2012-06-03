e0bbca10d961eb7a1fbd58738867e935632210b2
jimtahu <jimtahu@gmail.com>
2012-06-02 19:59:27 -0500

    Moved password generation to seperate class

--- src/android/jimtahu/passgen/PassGen.java
+++ src/android/jimtahu/passgen/PassGen.java
@@ -11,10 +11,8 @@ public class PassGen extends Activity{
     /** handles clicks */
     public void passgen(View v){
 	EditText output = (EditText) this.findViewById(R.id.output);
-	String txt = "";
-	for(int i=0; i<16; i++)
-	    txt = txt + (int)(Math.random()*10);
-	output.setText(txt);
+	Generator.seed(0x10);
+	output.setText(Generator.passcode());
     }; 
 
     /** Called when the activity is first created. */
